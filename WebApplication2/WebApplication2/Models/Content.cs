1a83ce96b61a63b2afcc9010eeb283f35d383566
xhma44 <xhma44@foxmail.com>
2019-01-23 10:14:41 +0800

    test

---
+++ WebApplication2/WebApplication2/Models/Content.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+    public class Content
+    {
+        public int Id { get; set; }
+        public string title { get; set; }
+        public string content { get; set; }
+        public int status { get; set; }
+        public DateTime add_time { get; set; }
+        public DateTime modify_time { get; set; }
+
+    }
+}
